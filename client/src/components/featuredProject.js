a33b07c0595dec0397753abac33ae9215544834c
willmas-here <65773478+willmas-here@users.noreply.github.com>
2022-06-19 20:57:11 -0700

    Fix Featured Project Card

--- client/src/components/featuredProject.js
+++ client/src/components/featuredProject.js
@@ -8,45 +8,54 @@ import Button from "./button"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function FeaturedProject() {
-    const data = useStaticQuery(
-        graphql`
-            query {
-                allMarkdownRemark(sort: {fields: frontmatter___date, order: ASC}, limit: 1) {
-                    nodes {
-                        frontmatter {
-                            title
-                            summary
-                        }
-                        fields {
-                            slug
-                            featuredImage {
-                                childImageSharp {
-                                    gatsbyImageData
-                                }
-                            }
-                        }
-                    }
-                }
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(
+          sort: { fields: frontmatter___date, order: DESC }
+          limit: 1
+        ) {
+          nodes {
+            frontmatter {
+              title
+              summary
             }
-        `
-    )
-    const frontmatter = data.allMarkdownRemark.nodes[0].frontmatter
-    const fields = data.allMarkdownRemark.nodes[0].fields
-    const { t } = useTranslation();
+            fields {
+              slug
+              featuredImage {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  const frontmatter = data.allMarkdownRemark.nodes[0].frontmatter
+  const fields = data.allMarkdownRemark.nodes[0].fields
+  const { t } = useTranslation()
 
-    return (
-        <div className={styles.container}>
-            <h2>{t("home:featuredProject.header", "My Latest Project")}</h2>
-            <div className={styles.projectContainer}>
-                <GatsbyImage className={styles.image} placeholder="blurred" image={fields.featuredImage.childImageSharp.gatsbyImageData} />
-                <h3 className={styles.title}>{frontmatter.title}</h3>
-                <p className={styles.summary}>{frontmatter.summary}</p>
-                <div className={styles.button}>
-                    <Button>
-                        <Link to={fields.slug}>{t("common:button.keepReading", "Keep Reading")}</Link>
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <h2>{t("home:featuredProject.header", "My Latest Project")}</h2>
+      <div className={styles.projectContainer}>
+        <GatsbyImage
+          className={styles.image}
+          placeholder="blurred"
+          image={fields.featuredImage.childImageSharp.gatsbyImageData}
+        />
+        <h3 className={styles.title}>{frontmatter.title}</h3>
+        <p className={styles.summary}>{frontmatter.summary}</p>
+        <div className={styles.button}>
+          <Button>
+            <Link to={fields.slug}>
+              {t("common:button.keepReading", "Keep Reading")}
+            </Link>
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
